267c8b069fb0e97db7d627cd350064f0bee61bc8
hyadav5 <hyadav5@CSCINDAI377289.asiapac.globalcsc.net>
2018-06-07 19:06:10 +0530

    bnew

--- JpaBootExample/src/main/java/com/dairy/controller/TransactionReportController.java
+++ JpaBootExample/src/main/java/com/dairy/controller/TransactionReportController.java
@@ -2,15 +2,19 @@ package com.dairy.controller;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.dairy.model.Ledger;
+import com.dairy.model.User;
 import com.dairy.service.TransactionReportService;
+import com.dairy.service.UserService;
 
 @Controller
 @RequestMapping("/transactionReport")
@@ -20,10 +24,13 @@ public class TransactionReportController {
 	@Autowired
 	TransactionReportService transactionReportService ;
 	
+	@Autowired
+	UserService userService ;
+	
 	    @RequestMapping(value={""}, method={RequestMethod.POST,RequestMethod.GET})	
 	    public String getAllTransactions(Model model) {
 	    	
-  	      return "transactionReport";
+  	      return "userTransactionReport";
 	    }
 	    
 	    
@@ -37,7 +44,7 @@ public class TransactionReportController {
 	    	
 	    	model.addAttribute("ledgers",legders);
 	    	
-  	      return "transactionReport";
+  	      return "userTransactionReport";
 	    }
 	    
 	    @RequestMapping(value={"/fromDateToNow"}, method={RequestMethod.POST,RequestMethod.GET})	
@@ -49,9 +56,34 @@ public class TransactionReportController {
 	    	
 	    	model.addAttribute("ledgers",legders);
 	    	
-  	      return "transactionReport";
+  	      return "userTransactionReport";
 	    }
 	    
+	    
+	    @RequestMapping(value={"/user/{id}"}, method={RequestMethod.POST,RequestMethod.GET})	
+	    public String userTransactionReport(User user ,@PathVariable Optional<Integer> id ,Model model) {
+	    	
+	    	if(id.isPresent())
+     	   { 
+     		  Integer userId = id.get();
+     		if(!userService.existsById(userId)) 
+     	     { 
+     		  model.addAttribute("result","Cannot find userId #" + userId);
+     	      return "userTransactionReport";
+     	     }
+     	   }
+	    	
+	    	LocalDateTime startDate =  LocalDateTime.of(2018, 05, 28, 15, 56); 
+	    	
+	    	/*List<Ledger> reports = transactionReportService.getTransationByDate(startDate, LocalDateTime.now());
+	    	*/
+	    	
+	        user = userService.getOne(id.get());
+	    	
+	    	model.addAttribute("user",user);
+	    	
+  	      return "userTransactionReport";
+	    }
 	  
 	    
       
