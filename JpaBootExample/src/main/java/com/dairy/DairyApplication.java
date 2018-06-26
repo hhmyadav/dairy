a85b3eef6fbf7e57f8bdcce7ccbabd883921d097
hyadav5 <hyadav5@CSCINDAI377289.asiapac.globalcsc.net>
2018-06-26 21:34:39 +0530

    off

--- JpaBootExample/src/main/java/com/dairy/DairyApplication.java
+++ JpaBootExample/src/main/java/com/dairy/DairyApplication.java
@@ -2,14 +2,20 @@ package com.dairy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
-public class DairyApplication {
+public class DairyApplication extends SpringBootServletInitializer{
     
 	
-	
+	 @Override
+	    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	    	return application.sources(DairyApplication.class);
+	    }
+	 
 	public static void main(String[] args) {
 		SpringApplication.run(DairyApplication.class, args);
 		
